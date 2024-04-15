47fb7277793e00172a1f61581f042bc810eda0fb
ds <dsmanohar222@gmail.com>
2024-04-15 21:30:59 +0530

    Addded the midi access to base in ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { error } from 'console'
+import { from } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
-  title = 'NoteMastery';
+export class AppComponent implements OnInit {
+  title = 'NoteMastery'
+  globalMidi: WebMidi.MIDIAccess | null = null
+
+  ngOnInit(): void {
+    from(navigator.requestMIDIAccess()).subscribe(
+      (data) => {
+        this.onMIDISuccess(data)
+      },
+      (error) => {
+        alert('You dont have midi access')
+      },
+    )
+  }
+
+  onMIDISuccess(midiAccess: WebMidi.MIDIAccess) {
+    if (midiAccess?.inputs.size > 0) {
+      this.globalMidi = midiAccess
+    } else {
+      this.onMIDIFailure()
+    }
+  }
+
+  onMIDIFailure() {
+    alert('You dont have midi access')
+  }
 }
